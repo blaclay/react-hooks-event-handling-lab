deb5ca88148df570cfcfbd6c442717ad6dadaee3
Clay Blackwelder <clayblackwelder@gmail.com>
2023-06-27 22:31:29 -0700

    Complete

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -2,8 +2,8 @@
 
 function Keypad (){
     return (
-        <div></div>
-    )
+        <div><input type="password" onChange={() => console.log("Entering password...")} /></div>
+    );
 }
 
 export default Keypad;
